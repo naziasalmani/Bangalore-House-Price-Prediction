615137eeeaaf6cfa1e6000998b32640d268e1925
Nazia <naziasalmani514@gmail.com>
2026-04-24 23:35:38 +0530

    fixed artifact path

--- server/util.py
+++ server/util.py
@@ -28,12 +28,12 @@ def load_saved_artifacts():
     global __locations
     global __data_columns
 
-    with open("./artifacts/columns.json", "r") as f:
+    with open("server/artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
     global __model
-    with open("./artifacts/banglore_home_prices_model.pickle", "rb") as f:
+    with open("server/artifacts/banglore_home_prices_model.pickle", "rb") as f:
         __model = pickle.load(f)
     print("Loading artifacts...done")
 
