5fb83a711fd2f0ac2525d9cdd2df9db6773ebdd2
Nazia <naziasalmani514@gmail.com>
2026-04-24 23:49:18 +0530

    fixed port issue for render

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 import util
 
 app = Flask(__name__)
@@ -40,4 +41,5 @@ def predict_home_price():
 
 if __name__ == '__main__':
     print("Starting Flask Server...")
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
